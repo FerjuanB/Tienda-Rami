e43b8906ea8a8d8aea050796ed7af17435d50be9
Fernando Juan Batres <fbatres@gmail.com>
2025-04-17 21:59:27 -0300

    enhanced the mobile grid columns up to 2, and updated the size of title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,15 +25,16 @@ export default function RootLayout({
       <body className={`${roboto.variable} ${roboto.className} bg-secondary-light`}>
         <header className="bg-gradient-to-r from-primary-dark to-primary-light text-white py-4 shadow-lg">
           <div className="container mx-auto px-4 flex justify-between items-center">
-            <img src="/logo.png" alt="Logo" className="w-24 h-24 rounded-lg" />
+            <img src="/logo.png" alt="Logo" className="w-16 h-16 sm:w-24 sm:h-24 rounded-lg" />
             
-            <span className="text-3xl font-semibold text-gray-200 uppercase">Tienda de regalos</span>
+            <p className="text-xl sm:text-3xl font-semibold text-gray-200 uppercase">Tienda de regalos</p>
           </div>
         </header>
         {children}
         <footer className="bg-primary-dark text-white py-6 mt-12">
           <div className="container mx-auto px-4 text-center">
             <p>© {new Date().getFullYear()} Hecho por <a href="https://github.com/FerjuanB" className="text-accent-yellow hover:text-accent-orange transition-colors">Fer Batres</a></p>
+            <p className="text-sm opacity-75 mt-2">Última actualización: {new Date().toLocaleString('es-AR', { timeZone: 'America/Argentina/Buenos_Aires', hour12: false })}</p>
             <p className="text-sm opacity-75 mt-2">Última actualización: {new Date().toLocaleString('es-AR', { timeZone: 'America/Argentina/Buenos_Aires' })}</p>
           </div>
         </footer>
--- src/components/product-grid.tsx
+++ src/components/product-grid.tsx
@@ -13,7 +13,7 @@ export default function ProductGrid({ products }: { products: Product[] }) {
 
   console.log('Products received in ProductGrid:', products);
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 [&>*]:min-[430px]:col-span-1">
       {products.map((product) => {
         console.log('Product being rendered:', product);
         
